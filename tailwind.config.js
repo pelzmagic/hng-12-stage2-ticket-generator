2b9134e3cb9c6a65715f7cebfd4054dde0e820c4
pelzmagic <akinloyepelz@gmail.com>
2025-02-13 18:03:58 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        jeju: ["JejuMyeongjo"],
+        roboto: ["Roboto"],
+        rage: ["Road Rage"],
+      },
+      colors: {
+        background: "#02191D",
+        cardColor: "#041E23",
+        secondary: "#08252B",
+        accent: "#12464E",
+        button: "#24A0B5",
+        white: "#ffffff",
+        btnText: "#0A0C11",
+        grey: "#FAFAFA",
+        deepgreen: "#0E464F",
+        darkgreen: "#052228",
+        deepergreen: "#07374F",
+        black: "#0E464F",
+      },
+    },
+  },
+  plugins: [],
+};
